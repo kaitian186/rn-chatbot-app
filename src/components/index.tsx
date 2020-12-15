ab40ed3993b9a88e00fc6eb350d8b911b7c8a4c2
Kai Tian <kaitian186@gmail.com>
2020-12-15 07:57:54 -0600

    empty back

--- src/components/index.tsx
+++
@@ -1,3 +0,0 @@
-export { Header } from "./Header";
-export { MessageList } from "./MessageList";
-export { MessageInputBox } from "./MessageInputBox";
--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const URL = 'https://gist.githubusercontent.com/pcperini/97fe41fc42ac1c610548cbfebb0a4b88/raw/cc07f09753ad8fefb308f5adae15bf82c7fffb72/cerebral_challenge.json';
-
-export enum USER_TYPE {
-  BOT = 'BOT',
-  USER = 'USER',
-}
